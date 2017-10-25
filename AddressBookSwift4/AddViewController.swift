13854ef1b75f851a34e19cac2a6c3109ca7b488a
Cocatrix <cocatrix@laposte.net>
2017-10-25 15:43:01 +0200

    Progress Bar

--- AddressBookSwift4/AddViewController.swift
+++ AddressBookSwift4/AddViewController.swift
@@ -16,11 +16,12 @@ class AddViewController: UIViewController {
     
     @IBOutlet weak var newFirstName: UITextField!
     @IBOutlet weak var newFamilyName: UITextField!
+    @IBOutlet weak var creatingProgressBar: UIProgressView!
     weak var delegate: AddViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        creatingProgressBar.setProgress(0, animated: false)
         // Do any additional setup after loading the view.
     }
 
@@ -30,9 +31,26 @@ class AddViewController: UIViewController {
     }
     
     @IBAction func clickedCreateContact(_ sender: Any) {
-        if let newFirstName = newFirstName.text, let newFamilyName = newFamilyName.text {
-            let newPerson = Person(firstName: newFirstName, familyName: newFamilyName)
-            delegate?.addPerson(newPerson: newPerson)
+        self.makeProgressOnBar()
+    }
+    
+    func makeProgressOnBar() {
+        //Async task : progress bar
+        DispatchQueue.global(qos: .userInitiated).async {
+            var percent: Float = 0
+            while percent < 1 {
+                Thread.sleep(forTimeInterval: 0.3)
+                DispatchQueue.main.async {
+                    self.creatingProgressBar.setProgress(percent + 0.1, animated: true)
+                    percent = self.creatingProgressBar.progress
+                }
+            }
+            DispatchQueue.main.async {
+                if let newFirstName = self.newFirstName.text, let newFamilyName = self.newFamilyName.text {
+                    let newPerson = Person(firstName: newFirstName, familyName: newFamilyName)
+                    self.delegate?.addPerson(newPerson: newPerson)
+                }
+            }
         }
     }
     
